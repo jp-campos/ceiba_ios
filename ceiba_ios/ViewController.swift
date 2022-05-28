37d8d270002043acf1fd8611fde425f3b22cb199
jpcampos <jpcampos@bancolombia.com.co>
2022-05-27 19:58:56 -0500

    Initial Commit

---
+++ ceiba_ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ceiba_ios
+//
+//  Created by juan.p.campos.duran on 27/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
